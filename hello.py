c7ac2303467557dcbb0dfb24573ad1849e281cc6
Merthon <imchenxing42@gmail>
2025-10-21 11:48:52 +0800

    feat: test

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
+from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"<p>Hello, {name}!</p>"
+
+@app.route("/api/info")
+def api_info():
+    data = {
+        "project": "helloAPI",
+        "version": "1.0",
+        "description": "A simple API test."
+    }
+    return jsonify(data)
 
 @app.route("/")
 def hello_world():
